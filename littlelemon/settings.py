ae8344c357fdab50868fda0856ed028e277e6ade
Arthur D'Aquino <arthur.daquino@mycodemedia.com>
2023-07-17 14:09:01 -0300

    set project database with mysql

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "littlelemon",
+    "restaurant",
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "littlelemon.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "littlelemon",
+        "USER": "root",
+        "PASSWORD": "rootroot",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
